add772903ac7d194bdc24e9626fe7942c3748038
alexandre.assad <alexandre.assad.pro@gmail.com>
2026-09-18 10:56:22 +0200

    :init: start project

---
+++ main.py
@@ -0,0 +1,11 @@
+import os 
+from pathlib import Path
+from domain.parsers.pdf_parser import pdf_processing
+
+def main():
+    print("Hello from school-rag-lab-1!")
+
+
+if __name__ == "__main__":
+    text = pdf_processing(Path("data/raw/pdf/Core Rules.pdf"), Path("data/processed/pdf/Core Rules.md"))
+    print(text)
